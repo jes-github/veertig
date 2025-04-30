e971ef1a84c9dbd4d726d9fa651039de98189e33
Pieter Vandereyken <pieter.vandereyken@jes.be>
2025-04-30 12:06:48 +0200

    updated mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,28 @@
-const menuToggle = document.getElementById("menu-toggle");
-const mobileMenu = document.getElementById("mobile-menu");
-const menuClose = document.getElementById("menu-close");
-const navbar = document.getElementById("navbar");
+document.addEventListener('DOMContentLoaded', function () {
+	const menuToggle = document.getElementById("menu-toggle");
+	const mobileMenu = document.getElementById("mobile-menu");
+	const menuClose = document.getElementById("menu-close");
+	const navbar = document.getElementById("navbar");
 
-// Open the menu
-menuToggle.addEventListener("click", function () {
-	mobileMenu.classList.remove("-translate-y-full");
-	mobileMenu.classList.add("translate-y-0");
-	navbar.classList.add("hidden");
-});
+	menuToggle.addEventListener("click", function () {
+		mobileMenu.classList.remove("-translate-y-full");
+		mobileMenu.classList.add("translate-y-0");
+		navbar.classList.add("hidden");
+	});
 
-// Close the menu
-menuClose.addEventListener("click", function () {
-	mobileMenu.classList.remove("translate-y-0");
-	mobileMenu.classList.add('-translate-y-full');
-	navbar.classList.remove("hidden")
-});
+	menuClose.addEventListener("click", function () {
+		mobileMenu.classList.remove("translate-y-0");
+		mobileMenu.classList.add("-translate-y-full");
+		navbar.classList.remove("hidden");
+	});
 
-// Close menu on link click (mobile)
-document.querySelectorAll('#mobile-menu a').forEach(link => {
-	link.addEventListener('click', () => {
-		mobileMenu.classList.remove('translate-y-0');
-		mobileMenu.classList.add('-translate-y-full');
-		navbar.classList.remove('hidden');
+	document.querySelectorAll('#mobile-menu a').forEach(link => {
+		link.addEventListener('click', () => {
+			mobileMenu.classList.remove('translate-y-0');
+			mobileMenu.classList.add('-translate-y-full');
+			navbar.classList.remove('hidden');
+		});
 	});
 });
+
 
